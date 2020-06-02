af51ed029d77a256f756c8b0008875b4eab4e5c1
ultrawide <colinchan02@gmail.com>
2020-06-02 15:58:32 -0700

    Bind port to environment otherwise 3000

--- app.js
+++ app.js
@@ -114,4 +114,7 @@ passport.deserializeUser(function(id, done) {
     });
 });
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
